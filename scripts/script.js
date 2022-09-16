4329da0a1b586ee25ada4682545601c1352d7d09
Gijs van de Putte <112858394+GijsvdP21@users.noreply.github.com>
2022-09-16 10:01:00 +0200

    Revert "Merge branch 'main' of https://github.com/GijsvdP21/your-tribe-profile-card"
    This reverts commit 81a8b96a44c5b6f9fb18122d47b6f47e09230f39, reversing
    changes made to 8dcfc528b8b55ae1c241e4588bc5d6a729e9208e.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-var container = document.querySelector("body")
+const expandButton = document.querySelector('header button')
+expandButton.addEventListener('click', expand)
+
+function expand () {
+  document.body.classList.toggle('expand')
+}
